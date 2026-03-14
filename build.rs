0c60a4a69910bb00b1b12ea4f76ff240cb141a8f
Runi-c <5150427+Runi-c@users.noreply.github.com>
2026-03-13 20:53:59 -0600

    add DM_OUT_DIR env var for controlling dm header writing

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 //! Buildscript which will save a `iconforge_rs.dm` with the DLL's public API.
 
-use std::{fs::File, io::Write};
+use std::{env, fs::File, io::Write, path::Path};
 
 macro_rules! feature_dm_file {
 	($name:expr) => {
@@ -9,7 +9,16 @@ macro_rules! feature_dm_file {
 }
 
 fn main() {
-	let mut f = File::create("target/iconforge_rs.dm").unwrap();
+	let target_dir = env::var("DM_OUT_DIR").unwrap_or_else(|_| String::from("target"));
+	if target_dir.trim().is_empty() {
+		return;
+	}
+	let dm_path = Path::new(&target_dir).join("iconforge_rs.dm");
+	let mut f = File::create(&dm_path).expect(&format!(
+		"Couldn't open `{}` for writing iconforge-rs DM headers. Set DM_OUT_DIR to an empty \
+		 string to disable writing headers or to an absolute path you want to write headers to.",
+		dm_path.display()
+	));
 
 	// header
 	writeln!(
