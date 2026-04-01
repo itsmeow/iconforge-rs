a47ab7cfcd381c7f589f2834b9c5aa2e9adc7067
itsmeow <itsmeow@itsmeow.dev>
2026-03-31 22:00:00 -0700

    (1.0.2) -> disable build.rs dm generation on cargo publish again

--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
 //! Buildscript which will save a `iconforge.dm` with the DLL's public API.
 
-use std::{env, fs::File, io::Write, path::PathBuf};
+use std::{
+	env,
+	fs::File,
+	io::Write,
+	path::{Path, PathBuf},
+};
 
 macro_rules! feature_dm_file {
 	($name:expr) => {
@@ -9,20 +14,24 @@ macro_rules! feature_dm_file {
 }
 
 fn main() {
-	if env::var("CARGO_PUBLISH").is_ok() {
+	let manifest_dir = env::var("CARGO_MANIFEST_DIR").expect("Manifest dir not set");
+	let manifest_path = Path::new(&manifest_dir);
+
+	// Disable generation with 'cargo publish' or 'cargo package'
+	if manifest_path
+		.components()
+		.any(|c| c.as_os_str() == "package")
+	{
 		return;
 	}
+
 	let dm_path = if let Ok(custom_dir) = env::var("DM_OUT_DIR") {
 		if custom_dir.trim().is_empty() {
 			return;
 		}
 		PathBuf::from(custom_dir).join("iconforge.dm")
 	} else {
-		let manifest_dir = env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set");
-
-		PathBuf::from(manifest_dir)
-			.join("target")
-			.join("iconforge.dm")
+		manifest_path.join("target").join("iconforge.dm")
 	};
 	let mut f = File::create(&dm_path).unwrap_or_else(|_| {
 		panic!(
